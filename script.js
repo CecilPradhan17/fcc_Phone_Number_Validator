8797127cc3b3c5958ce983b1903ce2e53dc014cc
CecilPradhan17 <cecilpradhan99@gmail.com>
2025-07-13 23:24:54 -0500

    layout changes

--- script.js
+++ script.js
@@ -3,29 +3,38 @@ const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const outputValue = document.getElementById("results-div");
 
-const checkNumber = () => {
+const addOutput = (validity,input) => 
+{
+  outputValue.innerHTML += `<br>${validity} US number: ` + input;
+}
+
+const checkNumber = () => 
+{
   const regex = /\D/g;
   const check = /^1?\s?(\d{3}|\(\d{3}\))[\s-]?\d{3}[\s-]?\d{4}/g;
   let input = (inputValue.value).trim();
+
   if (input === "")
     alert("Please provide a phone number");
+
   if (check.test(input))
   {
     const newInput = input.replace(regex,"");
+
     if(newInput.length === 10)
-      outputValue.innerHTML += "<br>Valid US number: " + input;
+      addOutput("Valid",input);
     else if(newInput.length === 11 && input[0] === '1')
-      outputValue.innerHTML += "<br>Valid US number: " + input;
+      addOutput("Valid",input);
     else
-      outputValue.innerHTML += "<br>Invalid US number: " + input;
+      addOutput("Invalid",input);
+
   }
-  else{
-    outputValue.innerHTML += "<br>Invalid US number: " + input;
-  }
-  
+  else
+    addOutput("Invalid",input);
 }
 
-const clearNumber = () => {
+const clearNumber = () => 
+{
   outputValue.textContent = "";
   inputValue.value = "";
 }
